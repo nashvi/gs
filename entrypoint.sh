0909e78d49ec5c88af2fa2e8b002ced1c44e3a62
nash <nash@iffy.me>
2019-10-01 15:51:06 +0800

    .#

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 if [ -z $1 ]; then
     exec /bin/bash
 elif [[ $1 == "-cu" ]]; then
-    /usr/local/bin/shadowsocks2-linux -c $2 \
+    /usr/local/bin/go-shadowsocks2 -c $2 \
         -verbose -socks :1080 -u -udptun :8053=8.8.8.8:53,:8054=8.8.4.4:53 \
                                  -tcptun :8053=8.8.8.8:53,:8054=8.8.4.4:53
 elif [[ $1 == "-c" ]]; then
-    /usr/local/bin/shadowsocks2-linux -c $2 \
+    /usr/local/bin/go-shadowsocks2 -c $2 \
         -verbose -socks :1080
 else
     exec "$@"
